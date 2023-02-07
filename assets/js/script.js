3c47c47b2f13ce2a4587ded9752eb9af34666378
Alexis <alexis_keeter@yahoo.com>
2023-02-06 19:43:04 -0700

    added outline for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// function for search button
+
+// function to fetch first API (job search)
+
+// console.log
+
+// function to display results
+
+// event listener
+
+// local storage
+
+// fetch second API (shibe)
+
+// console.log 
