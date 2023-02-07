f53e0d22e49e68a24758aa157553cd20c09060a6
Alexis <alexis_keeter@yahoo.com>
2023-02-06 20:24:39 -0700

    added shibe API to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,18 @@
 // local storage
 
 // fetch second API (shibe)
+function getShibe(shibe) {
+    var shibesUrl = 'http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]'
+    
+    fetch(shibesUrl)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log("shibe-fetch", data)
+            getShibe(data);
+        })
+}
+
 
 // console.log 
