2e500dccc848b72b4eb7487d879f1a69ac06cb39
MacKetchup <stylesthestyer@gmail.com>
2023-02-06 20:18:10 -0700

    set up api fetch function for job board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 // function for search button
 
 // function to fetch first API (job search)
+function getJobSearch(jobs) {
+    var jobsUrl = 'https://arbeitnow.com/api/job-board-api';
+
+    fetch(jobsUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+
+            console.log("job-fetch", data)
+            getJobSearch(data);
+        })
+}
 
 // console.log
 
